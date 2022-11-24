b1f51d212dd98bf8ecb3090417238d85c7bc2797
albertobaggio96 <albertobaggio96@gmail.com>
2022-11-24 16:12:03 +0100

    used .includes()

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-const button = document.querySelector("#dadi button");
+// Game dice 
+const diceElement = document.getElementById("dadi");
 
-const dadiElement = document.getElementById("dadi");
+const diceButton = diceElement.querySelector("button");
 
 const newDiv = document.createElement("div");
 newDiv.classList.add("col-12");
 
-button.addEventListener("click", function(){
+diceButton.addEventListener("click", function(){
   const myNumber = Math.floor(Math.random() * 6 + 1);
   const computerNumber = Math.floor(Math.random() * 6 + 1);
 
@@ -30,6 +31,27 @@ button.addEventListener("click", function(){
     <h2>${winner}</h2>
   </div>"`;
 
-  dadiElement.append(newDiv);  
-
+  diceElement.append(newDiv);  
 });
+
+// Check email
+const SectionEmailElement = document.getElementById("section-mail");
+
+const emailElement = document.getElementById("email");
+
+const emailButton = SectionEmailElement.querySelector("button");
+
+const gotEmail = ["pinco@gmail.com", "pallo@gmail.com", "caio@gmail.com", "sempio@gmail.com"];
+
+emailButton.addEventListener("click", function(){
+
+  const emailValue = emailElement.value.toLowerCase();
+  
+ if (gotEmail.includes(emailValue)){
+  console.log("esisto già");
+ }else{
+  console.log("non esisto già")
+ }
+})
+
+
